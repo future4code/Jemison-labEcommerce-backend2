dd2b827768aaedad53d7ddf518d61af1372488a4
Danielle Silveira Arantes <artdanielle@hotmail.com>
2022-11-29 21:30:09 -0300

    Endpoint - getAllCharacters e createCharacter

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {app} from "./app"
+import getAllCharacters from "./endpoints/getAllCharacters"
+import createCharacter from "./endpoints/createCharacter"
+
+
+app.get("/character", getAllCharacters)
+app.put("/character", createCharacter)
